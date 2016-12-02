2b1581fa3262f200b26326f5dbd1a71713f10864
Otso Björklund <otso.bjorklund@gmail.com>
2016-12-02 15:01:19 +0200

    Add memory class frame

---
+++ src/memory.py
@@ -0,0 +1,9 @@
+
+# Todo: Cedric toteuttaa
+
+class Memory():
+    pass
+    # -List etc. of motifs
+    # -Find most similar(motif)
+    # -memorize(motif), lisää muistiin uuden ja jos muisti on täynnä niin korvaa jonkin.
+    # -
